099a8076e30a60c9f0a6c93f02e0fdccf3d52913
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-06-27 22:43:59 +0900

    [feat] 프로그래머스: 순서바꾸기

---
+++ 알고리즘/코딩 기초 트레이닝/순서바꾸기.java
@@ -0,0 +1,18 @@
+//프로그래머스: 순서바꾸기
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        int idx = 0 ;
+        answer = new int[num_list.length];
+        
+        for(int i=n; i<num_list.length; i++){
+            answer[idx++] = num_list[i];
+        }
+        
+        for(int i=0; i<n; i++){
+            answer[idx++] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
