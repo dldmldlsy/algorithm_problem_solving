6f83022948f744b79ff52372bafe61432039c33c
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-08-07 23:53:50 +0900

    [feat] 백준31870: 버블버블

---
+++ 알고리즘/백준/백준31870_버블버블.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+
+        String[] arrStr = br.readLine().split(" ");
+        int[] arr = new int[n];
+        int[] arr2 = new int[n];
+        for(int i=0 ;i<n; i++) {
+            arr[i] = Integer.parseInt(arrStr[i]);
+            arr2[i] = Integer.parseInt(arrStr[i]);
+        }
+        int cnt =0;
+        int cnt2= 0;
+        int k=1;
+        int tmp;
+        while(k<n){
+            for(int i=0; i<=n-1-k; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                    cnt++;
+                }
+                if (arr2[i] < arr2[i+1]){
+                    tmp = arr2[i];
+                    arr2[i] = arr2[i + 1];
+                    arr2[i + 1] = tmp;
+                    cnt2++;
+                }
+            }
+            k++;
+        }
+
+        int answer = Math.min(cnt, cnt2+1);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Integer.toString(answer));
+        bw.flush();
+    }
+}
