8e45c203f56d3122ebba31093a25d7492a1c28ae
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-08-29 00:21:46 +0900

    [feat] 프로그래머스: 귤 고르기

---
+++ 알고리즘/프로그래머스/귤고르기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        Arrays.sort(tangerine);
+        int len = tangerine.length;
+        int maxSize = tangerine[len-1];
+        
+        int[] arr = new int[maxSize+1];
+        
+        for(int size : tangerine){
+            arr[size]++;
+        }
+        
+        Arrays.sort(arr);
+        
+        int sum = 0; //현재까지 담은 귤 수
+      
+        for(int i=maxSize; i>0; i--){
+            if(arr[i]==0){
+                continue;
+            }
+            if((sum+arr[i])<k){
+                sum+= arr[i];
+                answer++;
+            }else{
+                answer++;
+                break;
+            }
+        }
+        return answer;
+    }
+}
