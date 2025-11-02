3cd916417c6b1d9d5d5357f53648e7dc6944ad14
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-11-03 00:06:43 +0900

    [feat] 프로그래머스: 2016년

---
+++ 알고리즘/프로그래머스/2016년.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int total=0;
+        int[] year = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int i=1; i<a; i++){
+            total+=year[i];
+        }
+        total += b;
+        total--;
+        switch(total%7){
+            case 0: 
+                answer="FRI";
+                break;
+            case 1: 
+                answer="SAT";
+                break;
+            case 2: 
+                answer="SUN";
+                break;
+            case 3: 
+                answer="MON";
+                break;
+            case 4: 
+                answer="TUE";
+                break;
+            case 5: 
+                answer="WED";
+                break;
+            case 6: 
+                answer="THU";
+                break;
+        }
+            
+        return answer;
+    }
+}
