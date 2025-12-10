cc54ebad367068d7d1e170eae1a718f1450bfef5
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-12-10 18:46:46 +0900

    [refactor] 프로그래머스: k의 개수

--- 알고리즘/프로그래머스/k의 개수.java
+++ 알고리즘/프로그래머스/k의 개수.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        String target = ""; 
-        target += (char) (k+'0');
+        char target = (char) (k+'0');
         for(int num = i; num<=j; num++){
             String str = Integer.toString(num);
-            if(str.contains(target)){
-                for(int idx = 0; idx<str.length(); idx++){
-                    if(str.charAt(idx)==(char)(k+'0')){
-                        answer++;
-                    }
+            for(int idx = 0; idx<str.length(); idx++){
+                if(str.charAt(idx) == target){
+                    answer++;
                 }
             }
         }
